352548fc223b95a07e72f30a54d3627f216a1242
qkrtjsrb312 <chopr159@gmail.com>
2024-12-09 15:28:44 +0900

    feat:add api lostark character info

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { LostArkModule } from './lostark/lostark.module';
 @Module({
-  imports: [],
+  imports: [
+    LostArkModule,
+    ConfigModule.forRoot({isGlobal: true}),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ backend/src/lostark/lostark.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class LostArkService {
+  private readonly apiKey = process.env.LOSTARK_API_KEY;
+  private readonly apiUrl = 'https://developer-lostark.game.onstove.com'; // LostArk API 기본 URL
+
+  constructor(private readonly httpService: HttpService) {}
+
+  async getPlayerInfo(playerName: string) {
+    const url = `${this.apiUrl}/characters/${playerName}/siblings`;
+    console.log(url);
+    return this.httpService.get(url, {
+      headers: {
+        'accept': `application/json`,
+        'Authorization': `Bearer ${this.apiKey}`
+      },
+    })
+    .pipe(
+      map(response => response.data),
+    )
+    .toPromise();
+  }
+}
