a95736028fdbc5cb7bb19cd33cb974352bf4eed1
qkrtjsrb312 <chopr159@gmail.com>
2024-12-10 17:59:47 +0900

    fix: backend cors error

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173', // React 앱 주소
+    origin: '*', // React 앱 주소
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // 쿠키 사용 시 필요
   });
