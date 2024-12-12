2760151fe56e25cddf0ea0c1fc76e02c9f498213
qkrtjsrb312 <chopr159@gmail.com>
2024-12-12 16:51:23 +0900

    fix: change channel

--- frontend/src/components/layout/TopBar.tsx
+++ frontend/src/components/layout/TopBar.tsx
@@ -6,7 +6,7 @@ import Toolbar from "@mui/material/Toolbar";
 //import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
-import Logo from "../../../src/assets/logo.svg";
+import Logo from "../../assets/Logo.svg";
 
 export default function TopBar() {
   return (
