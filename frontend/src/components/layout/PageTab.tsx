86427cda59f56fb8b5e66a2274b7256a600bf41a
qkrtjsrb312 <chopr159@gmail.com>
2024-12-13 15:26:10 +0900

    feat: change tab position center

--- frontend/src/components/layout/PageTab.tsx
+++ frontend/src/components/layout/PageTab.tsx
@@ -4,7 +4,7 @@ import Tab from "@mui/material/Tab";
 import Box from "@mui/material/Box";
 
 export default function ColorTabs() {
-  const [value, setValue] = React.useState("one");
+  const [value, setValue] = React.useState("tab1");
 
   const handleChange = (_event: React.SyntheticEvent, newValue: string) => {
     setValue(newValue);
@@ -17,10 +17,12 @@ export default function ColorTabs() {
         onChange={handleChange}
         textColor="primary"
         indicatorColor="primary"
+        centered
       >
-        <Tab value="one" label="Item One" />
-        <Tab value="two" label="Item Two" />
-        <Tab value="three" label="Item Three" />
+        <Tab value="tab1" label="홈" />
+        <Tab value="tab2" label="캐릭터 정보" />
+        <Tab value="tab3" label="스펙업 최적화" />
+        <Tab value="tab4" label="군장검사" />
       </Tabs>
     </Box>
   );
