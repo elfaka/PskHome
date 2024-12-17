96a1c162b85e00c21c5d3e40a1c66a3e13b8716e
qkrtjsrb312 <chopr159@gmail.com>
2024-12-17 18:10:32 +0900

    refactor: move api logic to SearchForm

--- frontend/src/pages/infomation/page.tsx
+++ frontend/src/pages/infomation/page.tsx
@@ -4,24 +4,11 @@ import SearchForm from "./search/SearchForm";
 import CharacterInfo from "./search/CharacterInfo";
 import { ArmoryTotal } from "../../types/character/ArmoryTotal";
 
-export default function information() {
+export default function Information() {
   const [ArmoryTotal, setArmoryTotal] = useState<null | ArmoryTotal>(null); // API 결과 저장
-  const [error, setError] = useState<string | null>(null); // 에러 메시지 저장
 
-  const handleSearch = async (name: string) => {
-    setError(null); // 에러 초기화
-    setArmoryTotal(null); // 이전 결과 초기화
-
-    try {
-      const response = await fetch(`/api/lostark/character-info?name=${name}`);
-      if (!response.ok) {
-        throw new Error("API 호출 실패");
-      }
-      const data = await response.json();
-      setArmoryTotal(data); // API 결과 저장
-    } catch (err) {
-      setError("API 호출 중 오류가 발생했습니다.");
-    }
+  const handleResult = (data: ArmoryTotal) => {
+    setArmoryTotal(data); // API 결과 저장
   };
 
   return (
@@ -31,11 +18,10 @@ export default function information() {
         flexDirection: "column",
         alignItems: "center",
         textAlign: "center",
+        gap: 10,
       }}
     >
-      <h1>캐릭터 정보 검색</h1>
-      <SearchForm onSearch={handleSearch} />
-      {error && <p style={{ color: "red", marginTop: "10px" }}>{error}</p>}
+      <SearchForm onResult={handleResult} />
       <CharacterInfo ArmoryTotal={ArmoryTotal} />
     </Container>
   );
