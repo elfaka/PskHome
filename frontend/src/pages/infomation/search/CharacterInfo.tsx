63a63720639bdd0be2d4eae7768d913f6a09ddf4
qkrtjsrb312 <chopr159@gmail.com>
2024-12-19 10:42:45 +0900

    feat: split area

--- frontend/src/pages/infomation/search/CharacterInfo.tsx
+++ frontend/src/pages/infomation/search/CharacterInfo.tsx
@@ -1,6 +1,8 @@
 import Card from "@mui/material/Card";
-import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid2";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import { styled } from "@mui/material/styles";
 import { ArmoryTotal } from "../../../types/character/ArmoryTotal";
 interface ArmoryTotalProps {
   ArmoryTotal: ArmoryTotal | null;
@@ -9,37 +11,51 @@ interface ArmoryTotalProps {
 export default function CharacterInfo({ ArmoryTotal }: ArmoryTotalProps) {
   if (!ArmoryTotal) return null;
 
+  const Item = styled(Card)(({ theme }) => ({
+    backgroundColor: "#fff",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+    ...theme.applyStyles("dark", {
+      backgroundColor: "#1A2027",
+    }),
+  }));
+
   return (
-    <Grid container>
-      <Container>
-        <Card>
-          <img src={ArmoryTotal.ArmoryProfile.CharacterImage} />
-        </Card>
-      </Container>
-    </Grid>
-    // <div
-    //   style={{
-    //     marginTop: "20px",
-    //     textAlign: "left",
-    //     display: "inline-block",
-    //     maxHeight: "500px",
-    //     overflow: "auto",
-    //     padding: "10px",
-    //     border: "1px solid #ccc",
-    //     borderRadius: "4px",
-    //     width: "80%",
-    //   }}
-    // >
-    //   <h2>검색 결과:</h2>
-    //   <Card
-    //     style={{
-    //       backgroundColor: "#f4f4f4",
-    //       padding: "10px",
-    //       borderRadius: "4px",
-    //     }}
-    //   >
-    //     {JSON.stringify(characterInfo, null, 2)}
-    //   </Card>
-    // </div>
+    <Box>
+      <Grid container spacing={2}>
+        <Grid size={{ xs: 12, md: 5, lg: 4 }}>
+          <Item>
+            <img
+              src={ArmoryTotal.ArmoryProfile.CharacterImage}
+              style={{ width: "200px", height: "auto", borderRadius: "16px" }}
+            />
+          </Item>
+        </Grid>
+        <Grid container spacing={1} size={{ xs: 12, md: 7, lg: 8 }}>
+          <Grid size={{ xs: 6, lg: 4 }}>
+            <Item>
+              <Box>ArmoryTotal.ArmoryProfile.CharacterImage</Box>
+            </Item>
+          </Grid>
+          <Grid size={{ xs: 6, lg: 4 }}>
+            <Item></Item>
+          </Grid>
+          <Grid size={{ xs: 6, lg: 4 }}>
+            <Item></Item>
+          </Grid>
+          <Grid size={{ xs: 6, lg: 4 }}>
+            <Item></Item>
+          </Grid>
+          <Grid size={{ xs: 6, lg: 4 }}>
+            <Item></Item>
+          </Grid>
+          <Grid size={{ xs: 6, lg: 4 }}>
+            <Item></Item>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
