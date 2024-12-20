7515ca50fffe6b2b759bb3aa1fc27ad367aaa2cc
qkrtjsrb312 <chopr159@gmail.com>
2024-12-20 17:56:56 +0900

    feat: add special

--- frontend/src/pages/infomation/search/CharacterInfo.tsx
+++ frontend/src/pages/infomation/search/CharacterInfo.tsx
@@ -4,6 +4,7 @@ import { ArmoryTotal } from "../../../types/character/ArmoryTotal";
 import StyledItem from "../../../components/layout/StyledItem";
 import CharacterEquipment from "./CharacterEquipment";
 import CharacterAccessory from "./CharacterAccessory";
+import CharacterSpecial from "./CharacterSpecial";
 
 interface ArmoryTotalProps {
   ArmoryTotal: ArmoryTotal | null;
@@ -16,24 +17,29 @@ export default function CharacterInfo({ ArmoryTotal }: ArmoryTotalProps) {
 
   return (
     <Box>
-      <Grid container spacing={2}>
-        <Grid container spacing={1} size={{ xs: 12, md: 7, lg: 3 }}>
+      <Grid container spacing={1}>
+        <Grid container size={{ xs: 12, md: 7, lg: 3 }}>
           <CharacterEquipment equipmentList={ArmoryTotal.ArmoryEquipment} />
         </Grid>
-        <Grid size={{ xs: 12, md: 5, lg: 6 }}>
-          <StyledItem>
-            <img
-              src={ArmoryTotal.ArmoryProfile.CharacterImage}
-              style={{
-                width: "100%", // Card의 가로 크기에 맞춤
-                height: "100%", // Card의 세로 크기에 맞춤
-                objectFit: "cover", // 이미지가 찌그러지지 않도록 비율 유
-                borderRadius: "16px",
-              }}
-            />
-          </StyledItem>
+        <Grid container size={{ xs: 12, md: 5, lg: 6 }}>
+          <Grid size={{ xs: 12, md: 5, lg: 12 }}>
+            <StyledItem>
+              <img
+                src={ArmoryTotal.ArmoryProfile.CharacterImage}
+                style={{
+                  width: "100%", // Card의 가로 크기에 맞춤
+                  height: "100%", // Card의 세로 크기에 맞춤
+                  objectFit: "cover", // 이미지가 찌그러지지 않도록 비율 유
+                  borderRadius: "16px",
+                }}
+              />
+            </StyledItem>
+          </Grid>
+          <Grid container size={{ xs: 12, md: 5, lg: 12 }}>
+            <CharacterSpecial equipmentList={ArmoryTotal.ArmoryEquipment} />
+          </Grid>
         </Grid>
-        <Grid container spacing={1} size={{ xs: 12, md: 7, lg: 3 }}>
+        <Grid container size={{ xs: 12, md: 7, lg: 3 }}>
           <CharacterAccessory equipmentList={ArmoryTotal.ArmoryEquipment} />
         </Grid>
       </Grid>
