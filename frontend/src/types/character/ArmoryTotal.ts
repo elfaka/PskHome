a1b7f0ec4427acb6f64e9c468314c21495bb0a6c
qkrtjsrb312 <chopr159@gmail.com>
2024-12-24 16:37:16 +0900

    fix: AmortyTotal interface

--- frontend/src/types/character/ArmoryTotal.ts
+++ frontend/src/types/character/ArmoryTotal.ts
@@ -11,13 +11,13 @@ import { ColosseumInfo } from "./ColosseumInfo";
 
 export interface ArmoryTotal {
   ArkPassive: ArkPassive; // 아크 패시브
-  ArmoryAvatars: ArmoryAvatars; // 아바타
+  ArmoryAvatars: ArmoryAvatars[]; // 아바타
   ArmoryCard: ArmoryCard; // 카드
   ArmoryEngraving: ArmoryEngraving; // 각인
   ArmoryEquipment: ArmoryEquipment[]; // 장비
   ArmoryGem: ArmoryGem; // 보석
   ArmoryProfile: ArmoryProfile; // 캐릭터 정보
-  ArmorySkills: ArmorySkills; // 스킬
-  Collectibles: Collectibles; // 수집형 포인트
+  ArmorySkills: ArmorySkills[]; // 스킬
+  Collectibles: Collectibles[]; // 수집형 포인트
   ColosseumInfo: ColosseumInfo; //PVP
 }
