f4f234a1f84d5d91819c1fd345283ece58d71f40
qkrtjsrb312 <chopr159@gmail.com>
2025-01-03 18:01:41 +0900

    refactor:change framework nest to spring8

--- backend/src/main/java/kr/elfaka/lostark/character/feign/LostArkFeignClient.java
+++ backend/src/main/java/kr/elfaka/lostark/character/feign/LostArkFeignClient.java
@@ -3,15 +3,13 @@ package kr.elfaka.lostark.character.feign;
 import kr.elfaka.lostark.character.dto.ArmoryTotalDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "lostArkClient", url = "https://developer-lostark.game.onstove.com")
+@FeignClient(name = "open-api")
 public interface LostArkFeignClient {
     // Feign 메서드 정의
     @GetMapping("/armories/characters/{characterName}")
     ArmoryTotalDto getCharacterInfo(
-            @RequestHeader("Authorization") String authorization,
             @PathVariable("characterName") String characterName // 경로 변수 매핑
     );
 }
--- backend/src/main/java/kr/elfaka/lostark/character/service/LostArkService.java
+++ backend/src/main/java/kr/elfaka/lostark/character/service/LostArkService.java
@@ -3,7 +3,6 @@ package kr.elfaka.lostark.character.service;
 import kr.elfaka.lostark.character.dto.ArmoryTotalDto;
 import kr.elfaka.lostark.character.feign.LostArkFeignClient;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service // 필수: Spring이 Bean으로 인식
@@ -12,11 +11,7 @@ public class LostArkService {
 
     private final LostArkFeignClient lostArkFeignClient;
 
-    @Value("${lostark.api.key}")
-    private String apiKey;
-
     public ArmoryTotalDto getCharacterInfo(String characterName) {
-        String authorization = "Bearer " + apiKey;
-        return lostArkFeignClient.getCharacterInfo(authorization, characterName);
+        return lostArkFeignClient.getCharacterInfo(characterName);
     }
 }
