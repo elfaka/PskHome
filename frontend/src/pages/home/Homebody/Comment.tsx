7233ded402b130adeb79c9d113cb091f2b9f74a2
qkrtjsrb312 <chopr159@gmail.com>
2025-04-18 18:17:03 +0900

    feat: add comment

--- frontend/src/pages/home/Homebody/Comment.tsx
+++ frontend/src/pages/home/Homebody/Comment.tsx
@@ -1,7 +1,23 @@
 export default function Comment() {
   return (
     <div>
-      <h1>👋 만나서 반갑습니다. ELFAKA 입니다.</h1>
+      <h1 style={{ paddingTop: "20px", paddingBottom: "20px" }}>
+        👋 만나서 반갑습니다. ELFAKA 입니다.
+        <hr style={{ background: "#afafff", height: "3px" }} />
+      </h1>
+      <h3>
+        <ul>
+          <li>
+            해당 사이트는 그 동안 공부를 한 흔적을 기록하기 위해 만들었습니다.
+          </li>
+          <li>
+            <ul>
+              <li>111</li>
+            </ul>
+          </li>
+        </ul>
+      </h3>
+      <h2>Still Working...</h2>
     </div>
   );
 }
