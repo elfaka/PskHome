0f0785d8c0108484ae8ff0ef3e1cd4bb4790e8eb
qkrtjsrb312 <chopr159@gmail.com>
2025-04-22 16:16:51 +0900

    feat: add about css file

--- frontend/src/pages/home/about/about.tsx
+++ frontend/src/pages/home/about/about.tsx
@@ -1,21 +1,5 @@
-import Grid from "@mui/material/Grid2";
-import Homeheader from "../../../components/layout/Homeheader/Homeheader";
+import "./about.css";
 
 export default function About() {
-  return (
-    <Grid
-      container
-      spacing={10}
-      direction="column"
-      sx={{ height: "100vh", overflow: "hidden" }} // 화면 높이에 맞춤
-    >
-      {/* Homeheader: 고정 크기 */}
-      <Grid sx={{ flexShrink: 0 }}>
-        <Homeheader />
-      </Grid>
-
-      {/* HomeBody: 남은 영역을 차지하고 스크롤 가능 */}
-      <Grid sx={{ flexGrow: 1, overflowY: "auto" }}></Grid>
-    </Grid>
-  );
+  return <div></div>;
 }
--- frontend/src/pages/home/solve/solve.tsx
+++ frontend/src/pages/home/solve/solve.tsx
@@ -1,16 +1,5 @@
-import Homeheader from "../../../components/layout/Homeheader/Homeheader";
 import "./solve.css"; // 스타일은 따로 관리
 
 export default function Solve() {
-  return (
-    <div className="solve-container">
-      {/* Homeheader: 고정 영역 */}
-      <div className="solve-header">
-        <Homeheader />
-      </div>
-
-      {/* HomeBody: 나중에 본문 넣을 영역 */}
-      <div className="solve-body">{/* 여기에 본문 내용을 추가 */}</div>
-    </div>
-  );
+  return <div></div>;
 }
